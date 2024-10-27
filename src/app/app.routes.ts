de78edda7932f0955d343f2de0bf7996f6f57147
Sara <Sh6197174@gmail.com>
2024-10-27 22:05:37 +0200

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SearchRepositoriesComponent } from './components/search-repositories/search-repositories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:SearchRepositoriesComponent}
+];
