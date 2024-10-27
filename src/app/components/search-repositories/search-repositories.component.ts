462d96233dd8375ffd76cd3351d5afbb01e7d47b
Sara <Sh6197174@gmail.com>
2024-10-27 22:03:03 +0200

    add 2 components

---
+++ src/app/components/search-repositories/search-repositories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-repositories',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-repositories.component.html',
+  styleUrl: './search-repositories.component.scss'
+})
+export class SearchRepositoriesComponent {
+
+}
